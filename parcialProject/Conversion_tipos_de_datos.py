609fdea4c4f4d53b4a4fec9f59383b0131d50eef
Juan Imbachi <juanjoimbol@gmail.com>
2024-09-10 22:04:28 -0500

    Se añade el archivo Multilinea y se le agrega codigo

--- parcialProject/Conversion_tipos_de_datos.py
+++ parcialProject/Conversion_tipos_de_datos.py
@@ -1,13 +1,13 @@
-# Conversión de cadena a entero
+# Conversion de cadena a entero
 cadena_a_entero = "42"
 entero = int(cadena_a_entero)
 print(f"Cadena a entero: {entero} (Tipo: {type(entero)})")
 
-# Conversión de cadena a decimal
+# Conversion de cadena a decimal
 cadena_a_decimal = "3.14"
 decimal = float(cadena_a_decimal)
 print(f"Cadena a decimal: {decimal} (Tipo: {type(decimal)})")
 
-# Conversión de entero a cadena
+# Conversion de entero a cadena
 entero_a_cadena = str(entero)
 print(f"Entero a cadena: {entero_a_cadena} (Tipo: {type(entero_a_cadena)})")
---
+++ parcialProject/Multilineas_de_cadenas.py
@@ -0,0 +1,6 @@
+# Usando triple comillas para cadenas multilineas
+cadena_multilinea = """Este es un ejemplo
+de una cadena que ocupa
+varias lineas. Puedes incluir
+saltos de linea facilmente."""
+print(cadena_multilinea)
